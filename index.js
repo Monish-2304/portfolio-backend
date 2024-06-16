a30b509b900d2bf76a95587d1b98c83b0dacfe16
monish <monish2304@gmail.com>
2024-06-17 00:41:56 +0530

    cors

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://monish-s.netlify.app',
+    methods: 'POST',
+  }));
 app.use(bodyParser.json());
 
 app.post('/send-email', (req, res) => {
